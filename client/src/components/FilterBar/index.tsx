0dd7e7b2e0afddcf91c99e31f33df6df6219b759
minhhuylqd <minhhuy2810@gmail.com>
2022-10-17 03:40:09 +0300

    Add FilterBar

---
+++ client/src/components/FilterBar/index.tsx
@@ -0,0 +1,11 @@
+import Search from "./Search"
+
+const FilterBar = () => {
+  return (
+    <div className="w-full flex flex-col gap-4 justify-center items-center p-4 md:p-8">
+      <Search />
+    </div>
+  )
+}
+
+export default FilterBar
