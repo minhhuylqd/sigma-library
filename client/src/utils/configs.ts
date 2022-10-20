5a1b2498defae90bdd588da85458fdcc2ef8a852
minhhuylqd <minhhuy2810@gmail.com>
2022-10-20 09:21:59 +0300

    Minor config

--- client/src/utils/configs.ts
+++ client/src/utils/configs.ts
@@ -1,2 +1,3 @@
 export const BACKEND_URL = 'http://localhost:4000/api/v1'
 export const COVER_URL = 'https://covers.openlibrary.org/b/isbn/'
+export const RECOMMEND_SERVICE_URL = 'http://localhost:5000'
